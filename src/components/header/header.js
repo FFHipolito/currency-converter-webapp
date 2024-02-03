8ba0fa98de3163a63203c0269ecbf43ff40854fc
FernandoHipolito <fernandofhipolito@gmail.com>
2024-02-03 12:31:10 -0300

    create header with logo

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import "./header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <img src="../../../images/currency-converter-logo.png"></img>
+    </div>
+  );
+}
+
+export default Header;
